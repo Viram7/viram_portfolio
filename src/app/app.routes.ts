a9dbd31559ead707a4b65cc41c08f5e799577c62
Viram7 <kushwahavicky15@gmail.com>
2025-09-25 11:00:17 +0530

    update app.route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,9 @@ import { Hero } from './components/hero/hero';
 
 export const routes: Routes = [
 
-  {path:'',redirectTo:'hero',pathMatch:'full'},
+  // {path:'',redirectTo:'hero',pathMatch:'full'},
+  {path:'',component:Hero},
+
   {path:'hero',component:Hero},
 
 ];
